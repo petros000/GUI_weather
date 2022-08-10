0797f4e1b81c983d28e34cc663f7154254f8fe2a
Petr Surovtsev <petr.surovcev@gmail.com>
2022-08-10 13:31:46 +0300

    work forecast

--- gui_weather.py
+++ gui_weather.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit
-from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QGraphicsView
+from PyQt5 import uic, QtCore
 from weather import Weather
+import pyqtgraph as pg
 
 
 class GUIWeather(QMainWindow):
@@ -12,7 +13,11 @@ class GUIWeather(QMainWindow):
 
         self.city_name = self.findChild(QLineEdit, 'lineEdit')
         self.country_cod = self.findChild(QLineEdit, 'lineEdit_2')
-        self.label_res = self.findChild(QLabel, )
+        self.label_res = self.findChild(QLabel, 'label')
+        self.graphicsView = self.findChild(QGraphicsView, 'graphicsView')
+
+        self.graphics = pg.PlotWidget(self.graphicsView)
+        self.graphics.setGeometry(QtCore.QRect(0, 0, 330, 230))
 
         self.button_current_weather = self.findChild(QPushButton, 'pushButton')
         self.button_forecast_weather = self.findChild(QPushButton, 'pushButton_3')
@@ -22,14 +27,16 @@ class GUIWeather(QMainWindow):
         self.button_forecast_weather.clicked.connect(self.get_forecast_weather)
 
     def get_current_weather(self):
-        weather = Weather(self.city_name, self.country_cod.text())
+        weather = Weather(self.city_name.text(), self.country_cod.text())
         current_weather = weather.call_current_weather_data()
         self.label_res.setText(str(current_weather))
 
     def get_forecast_weather(self):
-        weather = Weather(self.city_name, self.country_cod)
+        weather = Weather(self.city_name.text(), self.country_cod.text())
         date_time, date_temp = weather.call_5_day_3hour_forecast_data()
-        self.label_res.setText(f'{date_time[0]} -> {date_temp[0]}')
+
+        self.graphics.clear()
+        self.graphics.plot(date_temp)
 
 
 def main():
